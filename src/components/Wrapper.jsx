a0793ec77cdc5cdc99c445bbea2b00a1efebd39c
Tijani Femi <olutimedia@gmail.com>
2023-05-24 19:02:53 +0100

    update readme

--- src/components/Wrapper.jsx
+++ src/components/Wrapper.jsx
@@ -1,14 +1,14 @@
-import { Outlet } from "react-router-dom";
-import Navbar from "./Layout/Navbar";
-import Sidebar from "./Layout/Sidebar";
+import { Outlet } from 'react-router-dom';
+import Navbar from './Layout/Navbar';
+import Sidebar from './Layout/Sidebar';
 
 const Wrapper = () => {
   return (
-    <div className="flex">
-      <div className="flex-none w-[280px] h-screen sticky left-0 top-0 overflow-y-auto bg-[#FFFBF7]">
+    <div className='flex'>
+      <div className='flex-none w-[280px] sticky left-0 top-0 overflow-y-auto bg-[#FFFBF7]'>
         <Sidebar />
       </div>
-      <div className="flex-1 bg-white">
+      <div className='flex-1 bg-white'>
         <div>
           <Navbar />
         </div>
